73cca0356f3acfc9b403038395e2bb68b2da1a5b
Fahim Ur Rahman <frahman66660@gmail.com>
2022-04-06 17:20:11 +0600

    Service section completion

--- Portfolio Practice/JS/main.js
+++ Portfolio Practice/JS/main.js
@@ -1,21 +1,35 @@
 /* CHANGE BACKGROUND HEADER */
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 
 /* SERVICES */
 
-const modalViews = document.querySelectorAll('..services_modal'),
-    modalBtns = document.querySelectorAll('.services_button'),
-    modalClose = document.querySelectorAll('.services_modal-close')
+const modalViews = document.querySelectorAll('.services_modal')
+const modalBtns = document.querySelectorAll('.services_button')
+const modalClose = document.querySelectorAll('.services_modal-close')
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /* MIXITUP FILTER PORTFOLIO */
 
